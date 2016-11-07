85646b7712e5a89e80701165fdc2b6b5d9e384ed
WBreeze168 <WBreeze168@gmail.com>
2016-11-07 16:55:22 +0800

    HW3

--- HW.java
+++ HW.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 /*
  * ID:B0444144
@@ -9,102 +7,83 @@ import java.util.Scanner;
 public class HW {
 
 	public static void main(String[] args) {
-		Scanner k = new Scanner(System.in);
-		System.out.println("input N(deck of cards):");
-		int nDeck = k.nextInt();
-		Deck deck = new Deck(nDeck);
-
-		deck.printDeck();
-
-		if (isAllCardsCorrect(deck.getAllCards(), nDeck)) {
-			System.out.println("Well done!");
-		} else {
-			System.out.println("Error, please check your sourse code");
+		//Scanner sc = new Scanner(System.in);
+		//System.out.println("input N(deck of cards):");
+		//String testn= sc.nextLine(); 
+		int nDeck=1;
+		Deck deck=new Deck(nDeck);
+		
+		deck.shuffle();
+		Card newCard=deck.getOneCard();
+		newCard.printCard();
+		Card newCard2=deck.getOneCard();
+		newCard2.printCard();
+		deck.shuffle();
+		if(isAllCardsCorrect(deck.getAllCards(),nDeck)){
+			if(!isShuffleWorking(deck,newCard,newCard2)){
+				System.out.println("All Card: Well done! But shufller is not working");
+			}else{
+				System.out.println("Well done!");
+			}
+			
+		}else{
+			System.out.println("All Card: Error, please check your sourse code");
 		}
-		k.close();
+
 	}
-
-	private static boolean isAllCardsCorrect(ArrayList<Card> allCards, int nDeck) {
-
-		boolean isCorrect = true;
-		;
-		HashMap<String, Integer> checkHash = new HashMap<String, Integer>();
-		for (Card card : allCards) {
-			int suit = card.getSuit();
-			int rank = card.getRank();
-			if (suit > 4 || suit < 1 || rank > 13 || rank < 1) {
-				isCorrect = false;
-				break;
-			}
-			if (checkHash.containsKey(suit + "," + rank)) {
-				checkHash.put(suit + "," + rank, checkHash.get(suit + "," + rank) + 1);
-			} else {
-				checkHash.put(suit + "," + rank, 1);
-			}
-
+	/*
+	 * This method is used for checking your result, not a part of your HW3
+	 */
+	private static boolean isShuffleWorking(Deck deck,Card newCard,Card newCard2){
+		deck.shuffle();
+		boolean isCorrect=true;
+		if(newCard.getSuit().equals(newCard2.getSuit()) &&
+				newCard.getRank()==newCard2.getRank()){
+					isCorrect=false;
+					return isCorrect;
 		}
-		if (checkHash.keySet().size() == 52) {
-			for (int value : checkHash.values()) {
-				if (value != nDeck) {
-					isCorrect = false;
-					break;
-				}
-			}
-		} else {
-			isCorrect = false;
+		for(int i=0;i<53;i++){
+			deck.getOneCard();
+		}
+		if(deck.nUsed!=1){
+			isCorrect=false;
 		}
 		return isCorrect;
 	}
+	
+	private static boolean isAllCardsCorrect(ArrayList<Card> allCards,int nDeck){
+		//check the output 
+		boolean isCorrect=true;;
+		HashMap <String,Integer> checkHash=new HashMap<String,Integer>();
+		for(Card card:allCards){
+			Card.Suit suit= card.getSuit();
+			int rank = card.getRank();
+			if(rank>13||rank<1){
+				isCorrect=false;
+				break;
+			}
+			if(checkHash.containsKey(suit+","+rank)){
+				checkHash.put(suit+","+rank, 
+						checkHash.get(suit+","+rank)+1);
+			}else{
+				checkHash.put(suit+","+rank, 1);
+			}
 
-}
-
-class Deck {
-	private ArrayList<Card> cards;
-
-	public Deck(int nDeck) {
-		cards = new ArrayList<Card>();
-		for (int n = 0; n < nDeck; n++) {
-			for (int x = 1; x < 5; x++) {
-				for (int y = 1; y < 14; y++) {
-					Card card = new Card(x, y);
-					cards.add(card);
+		}
+		if(checkHash.keySet().size()==52){
+			for(int value:checkHash.values()){
+				if(value!=nDeck){
+					isCorrect=false;
+					break;
 				}
 			}
+		}else{
+			isCorrect=false;
 		}
-
-	}
-
-	public void printDeck() {
-		for(Card pCard:cards){
-			pCard.printCard();
-		}
-
-	}
-
-	public ArrayList<Card> getAllCards() {
-		return cards;
-	}
+		return isCorrect;
+}
 }
 
-class Card {
-	private int suit; // Definition: 1~4, Clubs=1, Diamonds=2, Hearts=3,
-						// Spades=4
-	private int rank; // 1~13
 
-	public Card(int s, int r) {
-		suit = s;
-		rank = r;
-	}
 
-	public void printCard() {
-		System.out.println(suit + "," + rank);
-	}
 
-	public int getSuit() {
-		return suit;
-	}
-
-	public int getRank() {
-		return rank;
-	}
-}
